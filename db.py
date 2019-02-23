08fb08c3f76a906b25599fbc2689fb98c03eab03
Daniel <db_win10_login@outlook.com>
2019-02-23 20:23:41 +0000

    db.py - access to mysql database

---
+++ db.py
@@ -0,0 +1,35 @@
+from flask import jsonify
+import mysql.connector
+
+# IMPORTANT NOTES:
+# in the SDE, items (inventory items) information
+# is in the invTypes table.
+
+# objects (items in space, not in inventory) information
+# is in the invItems table.
+# invNames contains names for objects only
+
+# invTypes contains all (most?) pertinent information on 
+# inventory items
+
+# this script will use the convention item (inventory) and object (space)
+db = mysql.connector.connect(
+    host='localhost',
+    user='sdeuser',
+    passwd='password123',
+    database='sde'
+    )
+
+
+c = db.cursor()
+
+def dbQuery(query,params):
+    try:
+        c.execute(query,params)
+        a = c.fetchall() 
+    
+        return jsonify(a) if a else jsonify([])
+    except Exception as e:
+        print("Error lols: %s" % (str(e)))
+        return str(e)
+
