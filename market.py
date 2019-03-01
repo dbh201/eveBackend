0269bd6c70c1668277773d3cd50ee8a9ca360af7
Daniel <db_win10_login@outlook.com>
2019-03-01 23:24:23 +0000

    Removed unneeded dependencies and code. altered ESI requests to reflect change to ESI threading model

--- market.py
+++ market.py
@@ -1,9 +1,6 @@
 from flask import Blueprint
-import http.client
-from esi import ESI,ESI_VERSION
 from region import getRegionIDByRegionName
 from item import getItemIDByItemName
-API = http.client.HTTPSConnection("localhost")
 # IMPORTANT NOTES:
 # in the SDE, items (inventory items) information
 # is in the invTypes table.
@@ -21,27 +18,21 @@ _prefix = '/market'
 ###
 @_market.route('/buys/<int:regionID>/<int:itemID>/<int:pageNum>', methods=['GET'])
 def getMarketBuysByRegionIDItemID(regionID,itemID,pageNum):
-    ESI.request('GET',ESI_VERSION+'/markets/%i/orders/?datasource=tranquility&order_type=buy&page=%i&type_id=%i'%(regionID,pageNum,itemID))
-    a=ESI.getresponse().read()
-    return a
+    return app.ESI.getJSONResp('/markets/%i/orders/?datasource=tranquility&order_type=buy&page=%i&type_id=%i'%(regionID,pageNum,itemID))
 ###
 
 ###
 @_market.route('/buys/<string:regionName>/<int:itemID>/<int:pageNum>', methods=['GET'])
 def getMarketBuysByRegionNameItemID(regionName,itemID,pageNum):
     regionID=getRegionIDByRegionName(regionName).get_json()[0][0]
-    ESI.request('GET',ESI_VERSION+'/markets/%i/orders/?datasource=tranquility&order_type=buy&page=%i&type_id=%i'%(regionID,pageNum,itemID))
-    a=ESI.getresponse().read()
-    return a
+    return app.ESI.getJSONResp('/markets/%i/orders/?datasource=tranquility&order_type=buy&page=%i&type_id=%i'%(regionID,pageNum,itemID))
 ###
 
 ###
 @_market.route('/buys/<string:regionName>/<int:itemID>/<int:pageNum>', methods=['GET'])
 def getMarketBuysByRegionIDItemName(regionID,itemName,pageNum):
     itemID=getItemIDByItemName(itemName)
-    ESI.request('GET',ESI_VERSION+'/markets/%i/orders/?datasource=tranquility&order_type=buy&page=%i&type_id=%i'%(rID,pageNum,itemID))
-    a=ESI.getresponse().read()
-    return a
+    return app.ESI.getJSONResp('/markets/%i/orders/?datasource=tranquility&order_type=buy&page=%i&type_id=%i'%(rID,pageNum,itemID))
 ###
 
 ###
@@ -49,35 +40,27 @@ def getMarketBuysByRegionIDItemName(regionID,itemName,pageNum):
 def getMarketBuysByRegionNameItemName(regionName,itemName,pageNum):
     regionID=getRegionIDByRegionName(regionName).get_json()[0][0]
     itemID=getItemIDByItemName(itemName).get_json()[0][0]
-    ESI.request('GET',ESI_VERSION+'/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(regionID,pageNum,itemID))
-    a=ESI.getresponse().read()
-    return a
+    return app.ESI.getJSONResp('/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(regionID,pageNum,itemID))
 ###
 
 ###
 @_market.route('/sells/<int:regionID>/<int:itemID>/<int:pageNum>', methods=['GET'])
 def getMarketSellsByRegionIDItemID(regionID,itemID,pageNum):
-    ESI.request('GET',ESI_VERSION+'/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(regionID,pageNum,itemID))
-    a=ESI.getresponse().read()
-    return a
+    return app.ESI.getJSONResp('/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(regionID,pageNum,itemID))
 ###
 
 ###
 @_market.route('/sells/<string:regionName>/<int:itemID>/<int:pageNum>', methods=['GET'])
 def getMarketSellsByRegionNameItemID(regionName,itemID,pageNum):
     regionID=getRegionIDByRegionName(regionName).get_json()[0][0]
-    ESI.request('GET',ESI_VERSION+'/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(regionID,pageNum,itemID))
-    a=ESI.getresponse().read()
-    return a
+    return app.ESI.getJSONResp('/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(regionID,pageNum,itemID))
 ###
 
 ###
 @_market.route('/sells/<string:regionName>/<int:itemID>/<int:pageNum>', methods=['GET'])
 def getMarketSellsByRegionIDItemName(regionID,itemName,pageNum):
     itemID=getItemIDByItemName(itemName)
-    ESI.request('GET',ESI_VERSION+'/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(rID,pageNum,itemID))
-    a=ESI.getresponse().read()
-    return a
+    return app.ESI.getJSONResp('/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(rID,pageNum,itemID))
 ###
 
 ###
@@ -85,11 +68,5 @@ def getMarketSellsByRegionIDItemName(regionID,itemName,pageNum):
 def getMarketSellsByRegionNameItemName(regionName,itemName,pageNum):
     regionID=getRegionIDByRegionName(regionName).get_json()[0][0]
     itemID=getItemIDByItemName(itemName).get_json()[0][0]
-    ESI.request('GET',ESI_VERSION+'/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(regionID,pageNum,itemID))
-    a=ESI.getresponse().read()
-    return a
+    return app.ESI.getJSONResp('/markets/%i/orders/?datasource=tranquility&order_type=sell&page=%i&type_id=%i'%(regionID,pageNum,itemID))
 ###
-
-if __name__ == '__main__':
-    app.run(debug=False)
-
