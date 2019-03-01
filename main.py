922960cdb39f2a45dba08c565d097555300720b0
Daniel <db_win10_login@outlook.com>
2019-03-01 23:25:29 +0000

    added clean threading for server

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask,abort,jsonify
-import mysql.connector
-import http.client
+from flask import Flask,abort,jsonify,request
 import blueprints
 import atexit
+import esi
+import analyser_thread
 
 # IMPORTANT NOTES:
 # in the SDE, items (inventory items) information
@@ -17,19 +17,38 @@ import atexit
 
 # this script will use the convention item (inventory) and object (space)
 
-app = Flask(__name__)
-with app.app_context():
-    import analyser_thread as athread
-    athread.analyser = athread.Analyser()
-    athread.analyser.start()
-    atexit.register(athread.analyser.terminate())
-    print(athread.analyser.regions)
-blueprints.register_all(app)
+def create_app():
+    app = Flask(__name__)
+    blueprints.register_all(app)
 
+    def stop_all():
+        print("Stopping all threads...")
+        if(app.ESI.is_alive()):
+            app.ESI.terminate()
+            app.ESI.join(10)
+        if(app.ESI.is_alive()):
+            print("ESI timed out. :(")
 
+        if(app.analyser.is_alive()):    
+            app.analyser.terminate()
+            app.analyser.join(10)
+        if(app.ESI.is_alive()):
+            print("analyser timed out. :(")
+
+    def start_all():
+        app.analyser = analyser_thread.Analyser()
+        app.analyser.start()
+        app.ESI = esi.ESIConnection()
+        app.ESI.start()
+
+    start_all()
+    atexit.register(stop_all)
+    return app
+
+app = create_app()
 @app.route('/')
 def index():
-    return 'This is a local REST endpoint for the SDE database.'
+    return "API Status:<br/>ESI Thread: %s<br/>Analyser Thread: %s" % (app.ESI.is_alive(),app.analyser.is_alive())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,threaded=False)
