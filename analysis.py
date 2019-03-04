7f1ef1708ed658d200ec6e030a1b7c868265e045
Daniel <db_win10_login@outlook.com>
2019-03-04 15:54:45 +0000

    status function added. getSellOrders added.

--- analysis.py
+++ analysis.py
@@ -1,8 +1,7 @@
 from flask import Blueprint,jsonify
-from db import dbQuery
+from flask import current_app as app
 from item import getItemIDByItemName
 from region import getRegionIDByRegionName
-import analyser_thread  as at
 _analysis = Blueprint('_analysis',__name__)
 _prefix = '/analysis'
 
@@ -11,6 +10,41 @@ def getBuyOrders(regionName,itemName):
     regionID=getRegionIDByRegionName(regionName).get_json()[0][0]
     itemID=getItemIDByItemName(itemName).get_json()[0][0]
     try:
-        return jsonify(at.analyser.regions[regionID].rBuys[itemID])
+        return jsonify(app.analyser.regions[regionID].rBuys[itemID])
+    except KeyError:
+        return "[" + str(regionID)+", " + str(itemID) + "] was not found"
+
+@_analysis.route('/status/')
+def getStatus():
+    ret="""
+    Current analyser status: <br/>
+    <table style="border: 1px solid black">
+    <tr>
+    <th style="border: 1px solid black">Region</th>
+    <th style="border: 1px solid black">Types</th>
+    <th style="border: 1px solid black">Buy sync</th>
+    <th style="border: 1px solid black">Sell sync</th>
+    </tr>
+    """
+    for i in app.analyser.regions:
+        lentypes = len(app.analyser.regions[i].rTypes)
+        if lentypes == 0:
+            lentypes = 1
+        ret += '<th style="border: 1px solid black">'+app.analyser.regions[i].rName+"</th>"
+        ret += '<th style="border: 1px solid black">'+str(len(app.analyser.regions[i].rTypes))+"</th>"
+        buypcnt = len(app.analyser.regions[i].rBuys)/lentypes
+        sellpcnt = len(app.analyser.regions[i].rSells)/lentypes
+        ret += '<th style="border: 1px solid black">'+str(buypcnt)+"</th>"
+        ret += '<th style="border: 1px solid black">'+str(sellpcnt)+"</th>"
+
+    ret += "</table>"
+    return ret
+
+@_analysis.route('/sellorders/<string:regionName>/<string:itemName>/')
+def getSellOrders(regionName,itemName):
+    regionID=getRegionIDByRegionName(regionName).get_json()[0][0]
+    itemID=getItemIDByItemName(itemName).get_json()[0][0]
+    try:
+        return jsonify(at.analyser.regions[regionID].rSells[itemID])
     except KeyError:
         return "[" + str(regionID)+", " + str(itemID) + "] was not found"
