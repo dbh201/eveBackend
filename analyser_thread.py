da170cfae663c285e2354ce88369ffba3009ba90
Daniel <db_win10_login@outlook.com>
2019-03-13 19:14:49 +0000

    NONFUNCTIONAL: trying to support json import for analyser state saving

--- analyser_thread.py
+++ analyser_thread.py
@@ -5,7 +5,7 @@ from http.client import HTTPSConnection
 from time import time,sleep
 from time import sleep
 from analyser_region import AnalyserRegion
-from json import loads
+import analyser_region_json as json
 
 DEFAULT_REGION="Verge Vendor"
 
@@ -14,15 +14,22 @@ class Analyser(threading.Thread):
         super(Analyser,self).__init__()
         self.c=esicon
         #list of AnalyserRegion
-        self.regions=dict() #not sync'd yet
-        rID=getRegionIDByRegionName(DEFAULT_REGION)[0][0]
-        self.regions[rID]=AnalyserRegion(rID,DEFAULT_REGION,[])
+        try:
+            self.loadState()
+        except FileNotFoundError as ose:
+            print(ose)
+            print("Loading defaults...")
+            self.regions=dict() #not sync'd yet
+            rID=getRegionIDByRegionName(DEFAULT_REGION)[0][0]
+            self.regions[rID]=AnalyserRegion(rID,DEFAULT_REGION,[])
 
         self.updatePeriod=5*60
         self.typesUpdatePeriod=10*60
         self.lastTypesUpdate=0
         self.term=threading.Event()
-
+        self.suspend=threading.Event()
+        self.suspend.set() # don't sync up until we're ready
+        
     def addRegion(self,regionID):
         regionName=getRegionNameByRegionID(regionID)
         self.regions[regionID]=AnalyserRegion(
@@ -30,11 +37,23 @@ class Analyser(threading.Thread):
                 regionName,
                 []
                 )
+
     def terminate(self):
         print("Asking the analyser thread to terminate...")
         self.term.set()
     def removeRegion(self,regionID):
         self.regions.pop(regionID)
+
+    def saveState(self):
+        fd=open('./analyser.json','w')
+        json.dump(self.regions, fd, cls=json.RegionEncoder)
+        fd.close()
+
+    def loadState(self):
+        fd=open('./analyser.json','r')
+        regions_json=json.load(fd)
+        for rid in regions_json:
+            self.regions[rid]=AnalyserRegion(regions_json[rid])
 
     def updateAdjacentRegions(self):
         newRegions=getAdjacentRegionIDsByRegionID(self.regionID)
@@ -44,29 +63,44 @@ class Analyser(threading.Thread):
         for i in newRegions:
             if not i in self.regions:
                 self.addRegion(i)
-
+    def pause(self):
+        self.suspend.set()
+    def resume(self):
+        self.suspend.clear()
+    def status(self):
+        if self.suspend.is_set():
+            return "Suspended"
+        return "Active"
     def run(self):
         print("Thread started.")
         print("Regions: %s" % (str(self.regions)))
         while not self.term.is_set():
+            if(self.suspend.is_set()):
+                sleep(0.5)
+                continue
             if(self.lastTypesUpdate + self.typesUpdatePeriod < time()):
                 print("Updating type information for regions")
                 self.updateAllRelevantTypes()
                 self.lastTypesUpdate=time()
-            for region in self.regions:
-                if(self.regions[region].rLastBuysUpdate + self.updatePeriod < time()):
-                    print("Updating region buys %s" % (self.regions[region]))
-                    self.ESIUpdateBuyOrders(region)
-                    self.regions[region].rLastBuysUpdate=time() 
+                for region in self.regions:
+                    if(self.term.is_set()):
+                        return
+                    if(self.regions[region].rLastBuysUpdate + self.updatePeriod < time()):
+                        print("Updating region buys %s" % (self.regions[region]))
+                        self.ESIUpdateBuyOrders(region)
+                        self.regions[region].rLastBuysUpdate=time() 
 
-                    print("Updating region sells %s" % (self.regions[region]))
-                    self.ESIUpdateSellOrders(region)
-                    self.regions[region].rLastSellsUpdate=time() 
+                        print("Updating region sells %s" % (self.regions[region]))
+                        self.ESIUpdateSellOrders(region)
+                        self.regions[region].rLastSellsUpdate=time() 
             sleep(1)
         print("Analyser thread terminated.")
+        self.saveState()
 
     def updateAllRelevantTypes(self):
         for i in self.regions:
+            if self.term.is_set():
+                return
             self.regions[i].setTypes(self.ESIUpdateRelevantTypes(i))
             for t in self.regions[i].rTypes:
                 self.regions[i].rBuys
@@ -90,11 +124,12 @@ class Analyser(threading.Thread):
 
     def ESIUpdateOrders(self,regionID,order_type,dest_dict):
         for item in self.regions[regionID].rTypes:
+            if self.term.is_set():
+                return
             print("Updating item %i for %s" % (item, self.regions[regionID]))
             p=1
             while not self.term.is_set():
-                esiOrders='/markets/%i/orders/?datasource=tranquility&order_type=%s&page=%i&type_id=%i'
-                r=self.c.getJSONResp(esiOrders % (regionID,order_type,p,item))
+                r=self.c.getBuyOrders(regionID,p,item)
                 print("Retrieved page %i of length %i:\n%s" % (p,len(r),r))
                 if len(r) == 0:
                     break
